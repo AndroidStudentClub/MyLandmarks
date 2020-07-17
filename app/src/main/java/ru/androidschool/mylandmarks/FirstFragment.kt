8c9dcf658285e12908c14d6dcb1f26306221756d
Mikhail Valuyskiy <Mvaluyskiy@beeline.ru>
2020-07-17 18:17:07 +0300

    Добавлена возможность сделать фото

--- app/src/main/java/ru/androidschool/mylandmarks/FirstFragment.kt
+++ app/src/main/java/ru/androidschool/mylandmarks/FirstFragment.kt
@@ -3,6 +3,7 @@ package ru.androidschool.mylandmarks
 import android.Manifest
 import android.app.Activity
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -18,6 +19,8 @@ import androidx.navigation.fragment.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -103,6 +106,10 @@ class FirstFragment : Fragment() {
             preview = Preview.Builder()
                 .build()
 
+            // Добавили инициализацию ImageCapture UseCase
+            imageCapture = ImageCapture.Builder()
+                .build()
+
             // Select back camera
             val cameraSelector =
                 CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
@@ -111,10 +118,11 @@ class FirstFragment : Fragment() {
                 // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
 
-                // Bind use cases to camera
+                // Обновили биндинг к жизненному циклу
                 camera = cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview
+                    this, cameraSelector, preview, imageCapture
                 )
+
                 preview?.setSurfaceProvider(viewFinder.createSurfaceProvider())
             } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
@@ -123,9 +131,38 @@ class FirstFragment : Fragment() {
         }, ContextCompat.getMainExecutor(requireContext()))
     }
 
-    // Метод-заглушка для фото
     private fun takePhoto() {
-        Snackbar.make(rootView, "Заглушка для фото.", Snackbar.LENGTH_SHORT).show()
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create timestamped output file to hold the image
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(
+                FILENAME_FORMAT, Locale.US
+            ).format(System.currentTimeMillis()) + ".jpg"
+        )
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Setup image capture listener which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(requireContext()),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Ошибка сохранения фото: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Фото успешно сохранено: $savedUri"
+                    Snackbar.make(rootView, msg, Snackbar.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            })
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
